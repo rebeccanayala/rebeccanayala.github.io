26c85be93e5d7fd520bbef54233440fd43d6056e
Rebecca Nayala <rebeccanayala@gmail.com>
2021-10-26 22:20:31 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 const filter_btns = document.querySelectorAll(".filter-btn");
 
+filter_btns.forEach ((btn) => 
+  btn.addEventListener("click", () => {
+    filter_btns.forEach((button) => button.classList.remove("active"));
+    btn.classList.add("active");
+  })
+);
 
 $(".grid").isotope({
     itemSelector: ".grid-item",
